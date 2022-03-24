22726a502112b859455ebf0f1cab24b1e0ee69a6
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2022-03-24 19:31:33 -0400

    implement player sound

--- Assets/Scripts/State Machine/Transitions/HearsSound.cs
+++ Assets/Scripts/State Machine/Transitions/HearsSound.cs
@@ -5,20 +5,57 @@ using UnityEngine;
 public class HearsSound : Transition
 {
     public bool hearPlayerDebug = false;
+    private GameObject player = null;
+    private Main playerScript = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            Main script = player.GetComponent<Main>();
+            if (script != null)
+            {
+                playerScript = script;
+            } else
+            {
+                Debug.LogError("'Main' script not found on Player.");
+            }
+        } else
+        {
+            Debug.LogError("No player found in scene.");
+        }
         executeTransitionActions += transitionAction;
     }
 
     public override bool isTriggered()
     {
-        return hearPlayerDebug;
+        return canHearPlayer();
     }
 
     void transitionAction()
     {
         hearPlayerDebug = false;
+    }
+
+    bool canHearPlayer()
+    {
+        if (player != null)
+        {
+            float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+            if (distanceToPlayer <= playerScript.soundRadius)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
     }
 }
